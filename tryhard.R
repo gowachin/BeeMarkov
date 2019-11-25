9a9d4c94084a812854a8d331cd308dd948f0bc00
gowachin <gowachin974@gmail.com>
2019-11-25 11:38:14 +0100

    sweave begin

--- tryhard.R
+++ tryhard.R
@@ -1,16 +1,16 @@
 #### compiling ####
 setwd("manuscrit")
-Sweave("ICU_Report_Guerra_Jaunatre.Rnw")
-tools::texi2pdf("ICU_Report_Guerra_Jaunatre.tex") 
+Sweave("Markov_Report_Guerra_Jaunatre.Rnw")
+tools::texi2pdf("Markov_Report_Guerra_Jaunatre.tex") 
 file.remove(list.files()[ which(! list.files() %in% c("child_test.Rnw", 
-                                                      "fig","ICU.bib", 
-                                                      "ICU_Report_Guerra_Jaunatre.Rnw", 
-                                                      "ICU_Report_Guerra_Jaunatre.pdf",
-                                                      "ICU_Report_Guerra_Jaunatre_oldversion.Rnw") )]
+                                                      "fig","ICU.bib", "tex",
+                                                      "Markov_Report_Guerra_Jaunatre.Rnw", 
+                                                      "Markov_Report_Guerra_Jaunatre.pdf",
+                                                      "Markov_Report_Guerra_Jaunatre_oldversion.Rnw") )]
 )
 setwd("../")
 
-# system('xdg-open manuscrit/ICU_Report_Guerra_Jaunatre.pdf ') # works on ubuntu
+system('xdg-open manuscrit/Markov_Report_Guerra_Jaunatre.pdf ') # works on ubuntu
 
 
 ############################
